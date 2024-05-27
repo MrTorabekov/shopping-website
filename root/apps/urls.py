dd8421107a1309a2b5e475bdbdc6c698cd2fd1cc
torabekov-08 <diyorbektorabekov_08@gmail.com>
2024-05-27 09:48:34 +0500

    commit

--- root/apps/urls.py
+++ root/apps/urls.py
@@ -3,9 +3,9 @@ from .views import HomeView, LoginView,  AboutView, RegisterView,ProductView,con
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/', ProductView.as_view(), name='product'),
-    path('contact/', contact, name='contact'),
-    path('about/', AboutView.as_view(), name='about'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('product', ProductView.as_view(), name='product'),
+    path('contact', contact, name='contact'),
+    path('about', AboutView.as_view(), name='about'),
+    path('login', LoginView.as_view(), name='login'),
+    path('register', RegisterView.as_view(), name='register'),
 ]
